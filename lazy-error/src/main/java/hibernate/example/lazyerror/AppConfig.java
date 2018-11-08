2880fb9298faa219854734c410b88f9815976617
tumanau <tumanau@oksystem.cz>
2018-11-08 07:53:20 +0100

    Run cofigurace pro test

--- lazy-error/src/main/java/hibernate/example/lazyerror/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package hibernate.example.lazyerror;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
-
-@Configuration
-@EnableLoadTimeWeaving(aspectjWeaving = EnableLoadTimeWeaving.AspectJWeaving.ENABLED)
-public class AppConfig {
-}
